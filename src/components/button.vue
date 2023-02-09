79766253f476a56cba6fabc22aa1400c740d7177
Paul Le Grand <ptlegrand1@gmail.com>
2023-02-08 21:44:40 -0700

    Creating Vue 3 project, initial commit

---
+++ src/components/button.vue
@@ -0,0 +1,61 @@
+<script setup>
+// This is where any needed functionality will go
+// this component has none currently
+
+</script>
+
+<!-- This is where the form (html) of the component will go -->
+<template>
+  <div class="wrapper">
+    <button class="brutal-btn">
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<!-- This is where CSS will go -->
+<style>
+.brutal-btn {
+  background-color: white;
+  border: 4px solid black;
+  border-radius: 4px;
+  padding: 10px 25px;
+  font-weight: bold;
+  font-size: 20px;
+  position: relative;
+  color: black;
+}
+
+.brutal-btn:after {
+  content: '';
+  position: absolute;
+  background: black;
+  top: 8px;
+  left: 8px;
+  right: -8px;
+  bottom: -8px;
+  z-index: -1;
+}
+
+.brutal-btn:hover,
+brutalist-btn:active {
+  color: white;
+  background-color: black;
+  transition: all 0.3s ease;
+}
+
+.brutal-btn:hover:after,
+brutalist-btn:hover:active {
+  background-color: white;
+  border: 2px solid black;
+  transition: all 0.3s ease;
+}
+
+.wrapper {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,37 @@
+<script setup>
+import Button from './button.vue'
+
+// This is where any needed functionality will go
+// this component has none currently
+</script>
+
+<!-- This is where the form (html) of the component will go -->
+<template>
+  <div class="bg-gray-300 sticky border-gray-900 border-b-4">
+    <header>
+      <nav class="mx-auto max-w-7xl px-6 lg:px-8" aria-label="Top">
+        <div class="flex w-full items-center justify-between border-b border-gray-500 py-6 lg:border-none">
+          <div class="flex items-center">
+            <a href="#">
+              <span class="sr-only">Your Company</span>
+              <h1 class="uppercase text-4xl font-black">Find me lift tickets</h1>
+            </a>
+          </div>
+          <div class="ml-10 space-x-4">
+            <a href="/about" class="inline-block">
+              <Button>About</Button>
+            </a>
+            <a href="/faq" class="inline-block">
+              <Button>FAQ</Button>
+            </a>
+          </div>
+        </div>
+      </nav>
+    </header>
+  </div>
+</template>
+
+<!-- This is where CSS will go -->
+<style>
+
+</style>
