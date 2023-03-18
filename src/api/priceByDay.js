0c086ae639a626a1661ccc7b44eb9ea3a1fa0232
Jordan Watkins <watkinsconnor30@gmail.com>
2023-03-17 20:52:23 -0600

    added vue router

---
+++ src/api/priceByDay.js
@@ -0,0 +1,9 @@
+import fakePrices from './fakePricesByDay.json';
+
+function GetPriceByDay(month) {
+  // return axios.get('/get-prices-by-day', {
+  //   month: month,
+  // });
+
+  return fakePrices;
+}
